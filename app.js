e9d21afca0a019f0c737731b0f11c869187c5152
Sharavanakumar35 <shark.35.kumar@gmail.com>
2024-04-24 22:26:38 +0530

    origin host changed

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ import userRouter from "./Routers/users.router.js";
 const app = express();
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://jobconnect-crm.netlify.app/',
     credentials: true,
     sameSite: 'none'
 }));
