2b6cd230224b4da27205142114cba5ad3c4cbd2f
Sharavanakumar35 <shark.35.kumar@gmail.com>
2024-04-24 22:35:58 +0530

    cors issue try 1

--- app.js
+++ app.js
@@ -10,10 +10,16 @@ import userRouter from "./Routers/users.router.js";
 const app = express();
 
 app.use(cors({
-    origin: 'https://jobconnect-crm.netlify.app/',
+    origin: 'https://jobconnect-crm.netlify.app',
     credentials: true,
     sameSite: 'none'
 }));
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://jobconnect-crm.netlify.app');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+  });
 app.use(bodyParser.json({ limit: '10mb' }));
 app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }));
 app.use(express.json())
